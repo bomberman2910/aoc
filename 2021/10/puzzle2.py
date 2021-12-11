674f961f290fd9bb28d8a99f2f77ebdda2f8f724
Marcel Hasse <bomberman2995@gmail.com>
2021-12-11 16:55:17 +0100

    2021 Day 10 done

--- 2021/10/puzzle2.py
+++ 2021/10/puzzle2.py
@@ -1,52 +1,60 @@
 from queue import LifoQueue
 
-TEST = True
+TEST = False
 
 def puzzle(filecontent):
-    score = 0
+    scores = []
     for i in range(len(filecontent)):
         chunkstarts = LifoQueue()
         chars = list(filecontent[i])
+        error = False
         for j in range(len(chars)):
             if(chars[j] == '(' or chars[j] == '[' or chars[j] == '{' or chars[j] == '<'):
                 chunkstarts.put(chars[j])
             elif(chars[j] == ')'):
                 laststart = chunkstarts.get()
                 if(laststart != '('):
-                    if(j < len(chars) - 1):
-                        print("Error in line", i, "at position", j, "(expected ( from stack, got", laststart, "instead)")
-                    else:
-                        print("Line", i, "incomplete")
-                    score += 3
+                    print("Error in line", i, "at position", j, "(expected ( from stack, got", laststart, "instead)")
+                    error = True
                     break
             elif(chars[j] == ']'):
                 laststart = chunkstarts.get()
                 if(laststart != '['):
-                    if(j < len(chars) - 1):
-                        print("Error in line", i, "at position", j, "(expected [ from stack, got", laststart, "instead)")
-                    else:
-                        print("Line", i, "incomplete")
-                    score += 57
+                    print("Error in line", i, "at position", j, "(expected [ from stack, got", laststart, "instead)")
+                    error = True
                     break
             elif(chars[j] == '}'):
                 laststart = chunkstarts.get()
                 if(laststart != '{'):
-                    if(j < len(chars) - 1):
-                        print("Error in line", i, "at position", j, "(expected { from stack, got", laststart, "instead)")
-                    else:
-                        print("Line", i, "incomplete")
-                    score += 1197
+                    print("Error in line", i, "at position", j, "(expected { from stack, got", laststart, "instead)")
+                    error = True
                     break
             elif(chars[j] == '>'):
                 laststart = chunkstarts.get()
                 if(laststart != '<'):
-                    if(j < len(chars) - 1):
-                        print("Error in line", i, "at position", j, "(expected < from stack, got", laststart, "instead)")
-                    else:
-                        print("Line", i, "incomplete")
-                    score += 25137
+                    print("Error in line", i, "at position", j, "(expected < from stack, got", laststart, "instead)")
+                    error = True
                     break
-    print(score)
+        if(not error and not chunkstarts.empty()):
+            missing = list(chunkstarts.queue)
+            print("Line", i, "incomplete (Missing", missing, "in endings)")
+            score = 0
+            while not chunkstarts.empty():
+                char = chunkstarts.get()
+                score *= 5
+                if(char == '('):
+                    score += 1
+                elif(char == '['):
+                    score += 2
+                elif(char == '{'):
+                    score += 3
+                elif(char == '<'):
+                    score += 4
+            scores.append(score)
+    scores = sorted(scores)
+    middleindex = (len(scores) - 1) // 2
+    print(scores, middleindex)
+    print(scores[middleindex])
     pass
 
 def test():
