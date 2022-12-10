c5cc1ae33d865dec28f719332a800110f30083ca
Marcel Hasse <bomberman2995@gmail.com>
2022-12-10 14:51:28 +0100

    Added 2022 Day 10

---
+++ 2022/10/puzzle2.py
@@ -0,0 +1,57 @@
+TEST = False
+TEST_SOLUTION = 0 # add solution for test input here
+
+def puzzle(filecontent):
+    result = 0
+    # insert solution here
+
+    total_cycles = 0
+    for command in filecontent:
+        if(command == 'noop'):
+            total_cycles += 1
+        elif(command.startswith('addx')):
+            total_cycles += 2
+        
+    register_x = 1
+    is_in_add = False
+    current_add_value = 0
+    instruction_pointer = 0
+    lines = [[] for _ in range(6)]
+    for current_cycle in range(total_cycles):
+        line_position = current_cycle % 40
+        current_line = current_cycle // 40
+        if(line_position in range(register_x - 1, register_x + 2)):
+            lines[current_line].append('#')
+        else:
+            lines[current_line].append('.')
+        if(is_in_add):
+            register_x += current_add_value
+            instruction_pointer += 1
+            is_in_add = False
+            continue
+        if(filecontent[instruction_pointer].startswith('addx')):
+            current_add_value = int(filecontent[instruction_pointer].split(' ')[1])
+            is_in_add = True
+        else:
+            instruction_pointer += 1
+
+    for line in lines:
+        print(''.join(line))
+
+    return result
+
+def solve(input_filename):
+    file = open(input_filename, "r")
+    content = file.read().splitlines()
+    return puzzle(content)
+
+if(TEST):
+    testsolution = solve("test.txt")
+    if(testsolution == TEST_SOLUTION):
+        print("Solution for test input correct")
+        regularsolution = solve("input.txt")
+        print("Answer for main input", regularsolution)
+    else:
+        print(f"Solution for test input incorrect! (expected: {TEST_SOLUTION}; is: {testsolution})")
+else:
+    solve("input.txt")
