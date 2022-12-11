6b0358c194ba3c3c5e7fd23d75e4db42d6c2fb7c
Marcel Hasse <bomberman2995@gmail.com>
2022-12-11 21:31:24 +0100

    changed init.ps1

--- init.ps1
+++ init.ps1
@@ -12,9 +12,13 @@ $folder = ".\" + $year + "\" + $day
 if(-not (Test-Path -Path $folder))
 {
     mkdir -Path $folder
-    New-Item ($folder + "\test.txt")
-    New-Item ($folder + "\input.txt")
-    Copy-Item ".\template.py" ($folder + "\puzzle1.py")
-    Copy-Item ".\template.py" ($folder + "\puzzle2.py")
-    code $folder
+    $testpath = $folder + "\test.txt"
+    $inputpath = $folder + "\input.txt"
+    $puzzle1path = $folder + "\puzzle1.py"
+    $puzzle2path = $folder + "\puzzle2.py"
+    New-Item $testpath
+    New-Item $inputpath
+    Copy-Item ".\template.py" $puzzle1path
+    Copy-Item ".\template.py" $puzzle2path
+    code $folder $testpath $inputpath $puzzle1path $puzzle2path
 }
